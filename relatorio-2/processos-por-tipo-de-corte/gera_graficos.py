894b43c8e08621f69b6b1642e36cc8552affd275
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-02-01 02:32:15 -0200

    ADD: processos por tipo de corte

---
+++ relatorio-2/processos-por-tipo-de-corte/gera_graficos.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from os import remove
+from glob import glob
+from time import strftime
+from outputty import Table
+from plotter import Plotter
+
+
+def log(text):
+    print('[{}] {}'.format(strftime('%Y-%m-%d %H:%M:%S'), text))
+
+log('Normalizando dados...')
+table = Table(headers=['UF', u'Ordinária', 'Constitucional',
+                       'Recursal de Massa', 'Total'])
+for arquivo in glob('*.csv'):
+    uf = arquivo.replace('.csv', '')
+    t = Table()
+    t.read('csv', arquivo)
+    data = {'UF': uf, 'Total': 0}
+    for registro in t.to_list_of_dicts(encoding=None):
+        data[registro['corte']] = registro['processos']
+        data['Total'] += registro['processos']
+    table.append(data)
+table.order_by('Total', 'desc')
+del table['Total']
+table.write('csv', 'processos-por-tipo-de-corte.csv')
+
+log('Plotando gráficos...')
+p = Plotter('processos-por-tipo-de-corte.csv', width=1600, height=1200, rows=4,
+            cols=1)
+p.bar(x_column='UF', title=u'Processos - Recursal de Massa (todos os anos)',
+      bar_width=0.5, y_columns=['Recursal de Massa'], colors=['r'])
+p.bar(x_column='UF', title=u'Processos - Ordinária (todos os anos)',
+      bar_width=0.5, y_columns=[u'Ordinária'], colors=['g'])
+p.bar(x_column='UF', title=u'Processos - Constitucional (todos os anos)',
+      bar_width=0.5, y_columns=['Constitucional'], colors=['b'])
+p.bar(x_column='UF', title=u'Processos - todas as cortes (todos os anos)',
+      bar_width=0.5,
+      y_columns=['Recursal de Massa', u'Ordinária', 'Constitucional'],
+      colors=['r', 'g', 'b'])
+p.save('processos-por-tipo-de-corte.png')
+remove('processos-por-tipo-de-corte.csv')
+log('Done!')
