51c87bede9d2ab023b2649c3c018be9a36b55931
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-02-01 02:16:16 -0200

    ENH: adicionada função log() e + verbosidade

--- relatorio-2/processos_por_estado_por_ano/gera_graficos.py
+++ relatorio-2/processos_por_estado_por_ano/gera_graficos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from sys import stdout
+from time import strftime
 from os import mkdir, remove
 from shutil import rmtree
 from glob import glob
@@ -8,6 +10,13 @@ from collections import Counter
 from outputty import Table
 from plotter import Plotter
 
+
+def log(text, date_and_time=True):
+    if date_and_time:
+        stdout.write('[%s] %s' % (strftime('%Y-%m-%d %H:%M:%S'), text))
+    else:
+        stdout.write(text)
+    stdout.flush()
 
 def plota_graficos(arquivo, imagem, titulo, y_lim=(0, 160000)):
     p = Plotter(arquivo, rows=3, cols=1)
@@ -24,35 +33,39 @@ def plota_graficos(arquivo, imagem, titulo, y_lim=(0, 160000)):
     p.save(imagem)
 
 
-try:
-    rmtree('graficos')
-except OSError:
-    pass
-mkdir('graficos')
+if __name__ == '__main__':
+    try:
+        rmtree('graficos')
+    except OSError:
+        pass
+    mkdir('graficos')
 
-# Gráficos por ano:
-tabelas = []
-anos = []
-for arquivo in glob('processos-por-uf-????.csv'):
-    ano = arquivo.split('-')[-1].split('.')[0]
-    anos.append(int(ano))
-    p = plota_graficos(arquivo, 'graficos/' + arquivo.replace('.csv', '.png'),
-                       'Processos por UF de ' + ano)
-    tabela = Table()
-    tabela.read('csv', arquivo)
-    tabelas.append(tabela)
-
-# Gráfico consolidado:
-processos = Counter()
-for tabela in tabelas:
-    for registro in tabela.to_list_of_dicts():
-        processos[registro['uf']] += registro['processos']
-tabela = Table(headers=['uf', 'processos'])
-for item in processos.iteritems():
-    tabela.append(item)
-tabela.write('csv', 'processos-por-uf-geral.csv')
-plota_graficos('processos-por-uf-geral.csv',
-               'graficos/processos-por-estado-2000-a-2010.png',
-               'Processos por UF de {} a {}'.format(min(anos), max(anos)),
-               y_lim=(0, 1000000))
-remove('processos-por-uf-geral.csv')
+    log('Criando gráficos por ano... ')
+    tabelas = []
+    anos = []
+    for arquivo in glob('processos-por-uf-????.csv'):
+        ano = arquivo.split('-')[-1].split('.')[0]
+        anos.append(int(ano))
+        p = plota_graficos(arquivo,
+                           'graficos/' + arquivo.replace('.csv', '.png'),
+                           'Processos por UF de ' + ano)
+        tabela = Table()
+        tabela.read('csv', arquivo)
+        tabelas.append(tabela)
+    log('OK\n', date_and_time=False)
+    log('Criando gráfico consolidado... ')
+    processos = Counter()
+    for tabela in tabelas:
+        for registro in tabela.to_list_of_dicts():
+            processos[registro['uf']] += registro['processos']
+    tabela = Table(headers=['uf', 'processos'])
+    for item in processos.iteritems():
+        tabela.append(item)
+    tabela.write('csv', 'processos-por-uf-geral.csv')
+    plota_graficos('processos-por-uf-geral.csv',
+                   'graficos/processos-por-estado-2000-a-2010.png',
+                   'Processos por UF de {} a {}'.format(min(anos), max(anos)),
+                   y_lim=(0, 1000000))
+    remove('processos-por-uf-geral.csv')
+    log('OK\n', date_and_time=False)
+    log('Done!\n')
